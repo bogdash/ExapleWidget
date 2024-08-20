282abd9307d32f48456b2417ce438928f0cd7c4c
bogdash <ms.anastasia.bogdanova@gmail.com>
2024-08-20 12:07:54 +0300

    update: theme name

--- app/src/main/java/com/example/exaplewidget/ui/theme/Theme.kt
+++ app/src/main/java/com/example/exaplewidget/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun ExapleWidgetTheme(
+fun ExampleWidgetTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
